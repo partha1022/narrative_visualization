d35e9ae1a51d23ef7c65882fcb9bee15ad253cff
Partha Deb <partha.cse1@outlook.com>
2023-07-28 08:15:22 +0530

    commit

--- script.js
+++ script.js
@@ -125,7 +125,7 @@ function createScene1() {
 function createScene2() {
     // Create the SVG container for your visualization and append it to the #visualization div
     d3.select("#visualization-scene2").select("svg").remove();
-    svg_scene_1 = d3.select("#visualization-scene2")
+    svg_scene_2 = d3.select("#visualization-scene2")
         .append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
@@ -162,7 +162,7 @@ function createScene2() {
             .domain([0, d3.max(topData, d => +d.people_vaccinated) + 100000])
             .range([height, 0]);
 
-        svg_scene_1.append("text")
+        svg_scene_2.append("text")
         .attr("x", width / 2)
         .attr("y", 0 - margin.top / 2)
         .attr("text-anchor", "middle")
@@ -170,7 +170,7 @@ function createScene2() {
         .style("text-decoration", "underline")
         .text("Total vaccinationations by Country");
 
-        svg_scene_1.selectAll(".bar")
+        svg_scene_2.selectAll(".bar")
         .data(topData)
         .enter().append("rect")
         .attr("class", "bar")
@@ -183,7 +183,7 @@ function createScene2() {
         .text(d => `Persons Vaccinated: ${formatNumber(d.people_vaccinated)}`);
 
         // Add x-axis
-        svg_scene_1.append("g")
+        svg_scene_2.append("g")
             .attr("transform", `translate(0,${height})`)
             .call(d3.axisBottom(xScale))
             .selectAll("text")
@@ -193,7 +193,7 @@ function createScene2() {
             .attr("transform", "rotate(-65)");
 
         // Add y-axis
-        svg_scene_1.append("g")
+        svg_scene_2.append("g")
             .call(d3.axisLeft(yScale));
 
         
@@ -207,6 +207,94 @@ function createScene2() {
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Visualization scene 3
 function createScene3() {
+    d3.select("#visualization-scene3").select("svg").remove();
+
+    svg_scene_3 = d3.select("#visualization-scene3")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", `translate(${margin.left},${margin.top})`);
+
+
+    d3.csv(file_url).then(data => {
+        var countries = [...new Set(data.map(d => d.location))]
+        d3.select("#select-country").selectAll("option")
+              .data(countries)
+              .enter()
+                .append("option")
+                .text(d => d);
+        
+        var visualizations = ["Total Cases", "Total Deaths", "Total Vaccinations", "Patients in ICU", "Patients in hospital"]
+        d3.select("#select-viz").selectAll("option")
+              .data(visualizations)
+              .enter()
+                .append("option")
+                .text(d => d);
+
+        // When the button is changed, run the updateChart function
+        var countrySelect = d3.select("#select-country").on("change",  updateChart);
+        var parameterSelect = d3.select("#select-viz").on("change", updateChart);
+
+        // Function to update the chart
+        function updateChart() {  
+            var xScale = d3.scaleTime().range([0, width]);
+            var yScale = d3.scaleLinear().range([height, 0]);
+          
+            var selectedCountry = countrySelect.property("value");
+            var selectedViz = parameterSelect.property("value");
+            var selectedParameter = ""
+            
+            if (selectedViz == "Total Cases") {
+                selectedParameter = "total_cases"
+            } else if(selectedViz == "Total Deaths") {
+                selectedParameter = "total_deaths"
+            } else if(selectedViz == "Total Vaccinations") {
+                selectedParameter = "people_vaccinated"
+            } else if(selectedViz == "Patients in ICU") {
+                selectedParameter = "icu_patients"
+            } else {
+                selectedParameter = "hosp_patients"
+            }
+            
+
+            console.log(selectedCountry);
+            console.log(selectedParameter);
+
+            // Filter data based on selected country
+            var filteredData = data.filter(d => d.location === selectedCountry);
+
+            // Remove existing chart if there is one
+            d3.select("#visualization-scene3").selectAll("*").remove();
+
+            // Update scales
+            xScale.domain(d3.extent(filteredData, d => d.date));
+            yScale.domain([0, d3.max(filteredData, d => +d[selectedParameter])]);
+
+            // Create line generator
+            var line = d3.line()
+                .defined(d => !isNaN(d[selectedParameter]))
+                .x(d => xScale(d.date))
+                .y(d => yScale(d[selectedParameter]));
+            
+            console.log(line)
+
+            svg_scene_3.append("path")
+                    .datum(filteredData)
+                    .attr("fill", "none")
+                    .attr("stroke", "steelblue")
+                    .attr("stroke-width", 1.5)
+                    .attr("d", line);
+
+        }
+
+        // Initial chart with the first country and viz
+        updateChart();
+    
+    }).catch(error => {
+        console.log(error);
+        alert("An error occurred while loading the visualization.");
+    });  
 
 
 }
